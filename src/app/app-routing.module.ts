4d49b7a4cc1134c4ce40a40aaaf903f5ece34b96
Administrator <Administrator@YLMF-2019SGZAFL>
2019-04-09 19:43:08 +0800

    注释

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { HeroListComponent } from './hero-list/hero-list.component';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { RouterModule, Routes } from '@angular/router';
+import { PageNoFoundComponent } from './page-no-found/page-no-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'crisis-center', component: CrisisListComponent},
+  {path: 'heros', component: HeroListComponent},
+  {path: '', redirectTo: '/heroes', pathMatch: 'full'},
+  {path: '**', component: PageNoFoundComponent},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {enableTracing: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { PageNoFoundComponent } from './page-no-found/page-no-found.component';
+import { HeroListComponent } from './hero-list/hero-list.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrisisListComponent,
+    PageNoFoundComponent,
+    HeroListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
